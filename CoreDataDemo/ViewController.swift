9663116ad2c515192a59ba0bd6d3f0d8a92756b4
phineas_huang <phineas_huang@gemteks.com>
2017-01-18 13:45:48 +0800

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Phineas_Huang on 18/01/2017.
+//  Copyright © 2017 SunXiaoShan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
